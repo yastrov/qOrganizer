b093ae67c586fed7dfda527e79fd8b278ebed99d
Yuri Astrov <yuriastrov@gmail.com>
2017-05-28 22:12:51 +0300

    TreeItem has been added.

---
+++ src/model/treeitem.cpp
@@ -0,0 +1,111 @@
+#include "treeitem.h"
+
+TreeItem::TreeItem(const QString& name, int id, TreeItemType type, TreeItem *parent):
+    parentItem(parent), _name(name), _id(id), _type(type)
+{
+
+}
+
+TreeItem::~TreeItem()
+{
+    qDeleteAll(childItems);
+}
+
+TreeItemType TreeItem::type() const
+{
+   return _type;
+}
+
+void TreeItem::setType(TreeItemType type)
+{
+    _type = type;
+}
+
+int TreeItem::id() const
+{
+    return _id;
+}
+
+void TreeItem::setId(int id)
+{
+    _id = id;
+}
+
+QString TreeItem::name() const
+{
+    return _name;
+}
+
+QString TreeItem::setName(const QString &name)
+{
+    _name = name;
+}
+
+TreeItem *TreeItem::parent()
+{
+    return parentItem;
+}
+
+int TreeItem::childCount() const
+{
+    return childItems.count();
+}
+
+bool TreeItem::hasChild() const
+{
+    return !childItems.isEmpty();
+}
+
+int TreeItem::rowOfChild(TreeItem *child) const
+{
+    return childItems.indexOf(child);
+}
+
+int TreeItem::row() const
+{
+    if(parentItem) {
+        return parentItem->childItems.indexOf(const_cast<TreeItem*>(this));
+    }
+    return 0;
+}
+
+TreeItem *TreeItem::takeChild(int row)
+{
+    TreeItem *item = childItems.takeAt(row);
+    Q_ASSERT(item);
+    item->parentItem = nullptr;
+    return item;
+}
+
+void TreeItem::insertChild(int row, TreeItem *item)
+{
+    item->parentItem = this;
+    childItems.insert(row, item);
+}
+
+void TreeItem::appendChild(TreeItem *item)
+{
+    item->parentItem = this;
+    childItems << item;
+}
+
+int	TreeItem::columnCount() const
+{
+    return 1;
+}
+
+QVariant TreeItem::data(int column, int role) const
+{
+    if(column == 0 && (role == Qt::DisplayRole || role == Qt::EditRole) )
+        return _name;
+    return QVariant();
+}
+
+bool TreeItem::setData(int column, const QVariant &value, int role)
+{
+    if(column == 0 && role == Qt::EditRole) {
+        _name = value.toString();
+        return true;
+    }
+    return false;
+}
---
+++ src/model/treeitem.h
@@ -0,0 +1,47 @@
+#ifndef TREEITEM_H
+#define TREEITEM_H
+
+#include <QList>
+#include <QVariant>
+#include <QString>
+
+enum TreeItemType: int {
+    Folder = 0,
+    Task,
+    Notification
+};
+
+class TreeItem
+{
+public:
+    explicit TreeItem(const QString& name="Node", int id=0, TreeItemType type=TreeItemType::Folder, TreeItem *parent=nullptr);
+    virtual ~TreeItem();
+
+    TreeItemType type() const;
+    void setType(TreeItemType type);
+    int id() const;
+    void setId(int id);
+    QString name() const;
+    QString setName(const QString& name);
+    TreeItem *parent();
+    int childCount() const;
+    bool hasChild() const;
+    int rowOfChild(TreeItem *child) const;
+    int row() const;
+    TreeItem* takeChild(int row);
+    void insertChild(int row, TreeItem *item);
+    void appendChild(TreeItem *item);
+
+    virtual int	columnCount() const;
+    virtual QVariant data(int column, int role = Qt::DisplayRole) const;
+    virtual bool setData(int column, const QVariant &value, int role = Qt::EditRole);
+
+private:
+    QList<TreeItem*> childItems;
+    TreeItem *parentItem;
+    QString _name;
+    int _id;
+    TreeItemType _type;
+};
+
+#endif // TREEITEM_H
