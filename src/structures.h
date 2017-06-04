0cd0a030d34273f44f89ae38ca4cb262fc1d8d2c
Yuri Astrov <yuriastrov@gmail.com>
2017-06-04 16:45:20 +0300

    DateNotifyElement structure has been added.

---
+++ src/structures.h
@@ -0,0 +1,12 @@
+#ifndef STRUCTURES_H
+#define STRUCTURES_H
+
+#include <QDateTime>
+#include <QString>
+
+struct DateNotifyElement {
+    QDateTime dtime;
+    QString message;
+};
+
+#endif // STRUCTURES_H
